85ed4ca04d35070e81990fd3ab791f7658ab78f8
Tim Nelson <tim@eltimn.com>
2022-06-15 10:03:37 -0500

    Added implementation for workflow_dispatch

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ const parseLengthInput = s => {
 try {
   const shaLength = parseLengthInput(core.getInput('sha_length'))
 
-  console.log(`event_name: >${github.context.eventName}<`)
-
   if (github.context.eventName === 'pull_request') {
     const pullRequest = github.context.payload.pull_request
     const branch = slugify(pullRequest.head.ref)
@@ -24,24 +22,16 @@ try {
     core.setOutput('branch', branch)
     core.setOutput('sha', sha)
     console.log(`Output variables set for pull_request event: ${branch}-${sha}`)
-  } else if (github.context.eventName === 'push') {
+  } else if (github.context.eventName === 'push' || github.context.eventName === 'workflow_dispatch') {
     const parts = github.context.ref.split('/')
     const branch = slugify(parts[parts.length - 1])
     const sha = github.context.sha.slice(0, shaLength)
     core.setOutput('branch', branch)
     core.setOutput('sha', sha)
     console.log(`Output variables set for push event: ${branch}-${sha}`)
-  } else if (github.context.eventName.startsWith('workflow_dispatch')) {
-      const parts = github.context.ref.split('/')
-      const branch = slugify(parts[parts.length - 1])
-      const sha = github.context.sha.slice(0, shaLength)
-      core.setOutput('branch', branch)
-      core.setOutput('sha', sha)
-      console.log(`Output variables set for push event: ${branch}-${sha}`)
   } else {
-    console.log(`No implementation for event_namex: >${github.context.eventName}<`)
-    // core.setFailed(`No implementation for event_name: ${github.context.eventName}`)
+    core.setFailed(`No implementation for event_name: ${github.context.eventName}`)
   }
 } catch (error) {
-  core.setFailed(`Caught error: ${error.message}`)
+  core.setFailed(error.message)
 }
